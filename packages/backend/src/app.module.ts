0ce33d3ad28ab68aa47ef00db8c45a4cd05f0c8e
willy67k <willy67k001@gmail.com>
2026-03-10 16:56:53 +0800

    fix: module imports

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,15 +1,25 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 import dotenv from "dotenv";
 import { AppController } from "./app.controller.js";
 import { AppService } from "./app.service.js";
 import { DemoModule } from "./modules/demo/demo.module.js";
 import { BlockchainModule } from "./modules/blockchain/blockchain.module.js";
+import { DatabaseModule } from "./modules/database/database.module.js";
 
 const envFile = process.env.NODE_ENV === "production" ? ".env.production" : ".env.development";
 dotenv.config({ path: envFile });
 
 @Module({
-  imports: [DemoModule, BlockchainModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: envFile,
+    }),
+    DatabaseModule,
+    DemoModule,
+    BlockchainModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
